fd7a96dda22d96d7f11f8a5a167ee2f484da8c7b
Jamestyb <108416016+Jamestyb@users.noreply.github.com>
2023-11-20 22:40:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        700:{
+            items: 2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
