182e0b837407259d1fcd31ae38894ce8463e2774
Jamestyb <108416016+Jamestyb@users.noreply.github.com>
2023-11-23 23:15:16 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -16,3 +17,31 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+const btn = document.querySelectorAll('[data-tab-button]');
+for (let i=0; i<btn.length; i++){
+    btn[i].addEventListener('click', (btns)=>{
+        const buttonChar = btns.target.dataset.tabButton;
+        const charBtnShow = document.querySelector(`[data-tab-id=${buttonChar}]`)
+        rmvTab();
+        charBtnShow.classList.add('active');
+        rmvTabBtn();
+        btns.target.classList.add('btnActive');
+    })
+}
+
+function rmvTab(){
+    const tabs = document.querySelectorAll('[data-tab-id]')
+    for(let i = 0; i<tabs.length; i++){
+        tabs[i].classList.remove('active')
+    }
+}
+
+function rmvTabBtn(){
+    const btn = document.querySelectorAll('[data-tab-button]');
+    for(let i = 0; i<btn.length; i++){
+        btn[i].classList.remove('btnActive')
+    }
+}
